660de001a6166484cef0522da7aeb8c8c8ed437e
kyky0731 <97574818+kyky0731@users.noreply.github.com>
2022-03-15 13:04:25 -0700

    Update python.py

--- python.py
+++ python.py
@@ -4,10 +4,18 @@ class Dog:
     self.type = type
     self.is_ext = is_ext
     self.sci_name = sci_name
-  def show_info(self):
+  def show_info_dog(self):
     return f'The name of the dog is {self.name}. The type of the dog is a {self.type}, and it is {self.is_ext} that it is extinct. The scientific name is {self.sci_name}.'
-
+class Human:
+  def __init__(self, name, age, race, gender):
+    self.name = name
+    self.age = age
+    self.race = race
+    self.gender = gender
+  def show_info_human(self):
+    return f"""The name of the human is {self.name}. The race of the human is {self.race}. The age of the human is {self.age}. The gender of the human is a {self.gender}."""
 
 poodle = Dog('chloe', 'poodle', False, 'canus lupus familiaris')
-
-print(Dog.show_info(poodle))
+kyle = Human('Kyle', 12, 'Asian: Taiwanese', 'boy')
+print(Human.show_info_human(kyle))
+print(Dog.show_info_dog(poodle))
